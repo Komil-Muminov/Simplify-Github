966037d74e13b00d0e935ed6a00039623834adcd
Komil-Muminov <141549753+Komil-Muminov@users.noreply.github.com>
2024-04-09 13:31:50 +0500

    added imgs

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { products } from "../Products/CardData/Products";
 import { CardList } from "../Products/CardList/CardList";
-import RequsetLoading from "../Helpers/API/Loading/RequsetLoading";
 import "./Home.css";
 export const Home = () => {
 	return (
--- src/Components/Products/CardData/Products.js
+++ src/Components/Products/CardData/Products.js
@@ -1,41 +1,61 @@
 import { v4 as uuidv4 } from "uuid";
-// import image1 from "./img/1.jpg";
-import shopify from "./img/7.jpg";
-import shopify2 from "./img/11.jpg";
-import shopify3 from "./img/12.jpg";
-import shopify4 from "./img/13.jpg";
+
+import shopify5 from "./img/20.jpg";
+import shopify6 from "./img/21.png";
+import shopify7 from "./img/23.png";
+import shopify9 from "./img/28.jpg";
+import shopify10 from "./img/30.jpg";
+import shopify11 from "./img/24.jpg";
+import shopify12 from "./img/36.jpg";
+import shopify13 from "./img/46.jpg";
 // import shopify5 from "./img/15.jpg";
 export const products = [
 	{
 		id: uuidv4(),
-		img: shopify,
-		title: "Чой",
-		desc: "Сиех",
-	},
-
-	{
-		id: uuidv4(),
-		img: shopify2,
-		title: "Чой",
-		desc: "Сиех",
-	},
-
-	{
-		id: uuidv4(),
-		img: shopify3,
+		img: shopify9,
 		title: "Чой",
 		desc: "Сиех",
 	},
 	{
 		id: uuidv4(),
-		img: shopify4,
+		img: shopify10,
 		title: "Чой",
 		desc: "Сиех",
 	},
-	// {
-	// 	id: uuidv4(),
-	// 	img: shopify5,
-	// 	title: "Чой",
-	// 	desc: "Сиех",
-	// },
+	{
+		id: uuidv4(),
+		img: shopify11,
+		title: "Чой",
+		desc: "Сиех",
+	},
+	{
+		id: uuidv4(),
+		img: shopify6,
+		title: "Чой",
+		desc: "Сиех",
+	},
+	{
+		id: uuidv4(),
+		img: shopify7,
+		title: "Чой",
+		desc: "Сиех",
+	},
+	{
+		id: uuidv4(),
+		img: shopify5,
+		title: "Чой",
+		desc: "Сиех",
+	},
+	{
+		id: uuidv4(),
+		img: shopify12,
+		title: "Чой",
+		desc: "Сиех",
+	},
+	{
+		id: uuidv4(),
+		img: shopify13,
+		title: "Чой",
+		desc: "Сиех",
+	},
 ];
